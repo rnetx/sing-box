c7b0c07d6d098d95c24c4f194935ee9fb8108f13
CHIZI-0618 <kay9925@outlook.com>
2023-10-13 11:14:17 +0800

    proxyprovider: fix base64 decode

--- proxyprovider/raw/type.go
+++ proxyprovider/raw/type.go
@@ -16,11 +16,26 @@ type RawInterface interface {
 	Options() *option.Outbound
 }
 
+func base64Decode(b64 string) ([]byte, error) {
+	b64 = strings.TrimSpace(b64)
+	stdb64 := b64
+	if pad := len(b64) % 4; pad != 0 {
+		stdb64 += strings.Repeat("=", 4-pad)
+	}
+
+	b, err := base64.StdEncoding.DecodeString(stdb64)
+	if err != nil {
+		return base64.URLEncoding.DecodeString(b64)
+	}
+	return b, nil
+}
+
 func ParseRawConfig(raw []byte) ([]option.Outbound, error) {
 	rawStr := string(raw)
-	rawStr = strings.TrimSpace(rawStr)
-	_raw, err := base64.URLEncoding.DecodeString(rawStr)
-	if err == nil {
+	_raw, err := base64Decode(rawStr)
+	if err != nil {
+		return nil, err
+	} else {
 		rawStr = string(_raw)
 	}
 	rawList := strings.Split(rawStr, "\n")
