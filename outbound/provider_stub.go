04e814a5479abc8a1214c67f5c8af5caaeec0c35
0xffffharry <95022881+0xffffharry@users.noreply.github.com>
2023-12-20 21:10:12 +0800

    Fix bugs

--- outbound/provider_stub.go
+++ outbound/provider_stub.go
@@ -11,6 +11,6 @@ import (
 	E "github.com/sagernet/sing/common/exceptions"
 )
 
-func N1ewProvider(ctx context.Context, router adapter.Router, logFactory log.Factory, logger log.ContextLogger, tag string, options option.ProviderOutboundOptions) (adapter.Outbound, error) {
+func NewProvider(ctx context.Context, router adapter.Router, logFactory log.Factory, logger log.ContextLogger, tag string, options option.ProviderOutboundOptions) (adapter.Outbound, error) {
 	return nil, E.New(`Provider is not included in this build, rebuild with -tags with_provider`)
 }
